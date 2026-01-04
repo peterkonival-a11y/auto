2b15fbe94bb5730690b2e68ab4006e150eab114a
peterkonival-a11y <peter.konival@gmail.com>
2026-01-04 02:24:34 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE="ev-diesel-cache-v1";
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./"])));
-});
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
