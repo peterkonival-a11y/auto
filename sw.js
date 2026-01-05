efe0a0e5e8d020bf01430bb6bf018f3c8eb2fbd8
peterkonival-a11y <peter.konival@gmail.com>
2026-01-05 11:16:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,25 @@
-const CACHE="ev-diesel-cache-v1";
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./"])));
+const CACHE_NAME = 'fuel-tracker-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/android-chrome-192x192.png',
+  './icons/android-chrome-512x512.png',
+  './icons/apple-touch-icon.png'
+];
+
+// Inštalácia SW a cachovanie
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+// Fetch event – slúi offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
